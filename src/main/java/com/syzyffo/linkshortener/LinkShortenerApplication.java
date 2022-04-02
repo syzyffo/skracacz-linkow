61e3072c1b1b154cbfc7d198ca5deb261bbd843f
syzyffo <syzyffo@users.noreply.github.com>
2022-04-02 15:38:03 +0200

    Add spring project

---
+++ src/main/java/com/syzyffo/linkshortener/LinkShortenerApplication.java
@@ -0,0 +1,18 @@
+package com.syzyffo.linkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApplication {
+
+
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkShortenerApplication.class, args);
+	}
+
+}
